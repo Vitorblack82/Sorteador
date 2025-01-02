dd2e1e83d2d2ad009d687e3cd5c10cc8039d1eab
Vitorblack82 <apvitor@gmail.com>
2025-01-02 20:54:55 -0300

    alteração

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 function generateNumber() {
 
-    const um =Math.ceil(document.querySelector(".input-1").value)
+    const um = Math.ceil(document.querySelector(".input-1").value)
     const dois = Math.floor(document.querySelector(".input-2").value)
 
-    const result=  Math.floor(Math.random() * (dois - um) + um);
+    const result = Math.floor(Math.random() * (dois - um) + um);
 
-alert(result)
+    alert(result)
 
-    
+
 }
 
 
